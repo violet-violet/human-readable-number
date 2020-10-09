f5a6941c592be192da9f30974a29429c7f294e66
E. Demidovskaia <violet7693@yandex.ru>
2020-10-09 17:56:16 +0300

    feat: add code for numbers < 100

--- src/script.js
+++ src/script.js
@@ -5,35 +5,52 @@ function toReadable (number) {
     const numderArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
     'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'ninteen'];
 
-    const dozensArr = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninty', 'hundred'];
+    const dozensArr = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninty']; // , 'hundred'
     
-//     if (number < 20) {
-//         return `${numderArr[number]}`;
-//     }  
+    let numberPartToReturn = '';
 
-//     switch (numberStr) {
-//         case 2:
-//             console.log(numberStr);
-//             dozen = dozensArr[((number / 10) ^ 0) - 2];
-//             return `${dozen}`;
-//             // break;
+    // const dozenPartToReturn = dozensArr[((number / 10) ^ 0) - 2];
 
-//         case 3: 
-//             console.log(numberStr);
-//             break;
+    let dozenPartToReturn = function (x) {
+        if (x > 19) {
+            return dozensArr[((x / 10) ^ 0) - 2];
+        } else {
+            return '';
+        }        
+    };
+
+    // let numberPartToReturn = function (num) {
+    //     if ((num % 10) !== 0) {
+    //         numberPart = numderArr[num % 10]; 
+    //     } else { return '';}
+    // }
+
+    // console.log(number - numberStr[0] * 100);
+
     
-//         default:
-//             break;
-//     }
-    
-//     console.log(numderArr);
-// }
+    if (number < 20) {      
+        return `${numderArr[number]}`;
 
-for (let index = numberStr.length; index = 0; index--) {
-    const element = array[index];
+    }  else if (number < 100) {  
+
+        if ((number % 10) !== 0) {
+            numberPartToReturn = numderArr[number % 10]; 
+        }
+        return `${dozenPartToReturn(number)} ${numberPartToReturn}`;
+
+        } else if (number < 1000) {
+            const dozenFromHundred = number - (numberStr[0] * 100);
+            // console.log('dozenFromHundred: ', dozenFromHundred);
+
+            return `${numberPartToReturn} hundred ${dozenPartToReturn(dozenFromHundred)} ${numberPartToReturn}`;
+            }
+
+
+// for (let index = numberStr.length; index = 0; index--) {
+//     const element = array[index];
     
 }
 
-console.log(toReadable(19));
-console.log(toReadable(75));
-console.log(toReadable(111));
+console.log(toReadable(19), '- результат. Должно быть ninteen');
+console.log(toReadable(75), '- результат. Должно быть seventy five');
+console.log(toReadable(111), '- результат. Должно быть one hundred eleven');
